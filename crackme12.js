c6644b35c0f2023ee7dea6a66a96d3af1ab4b8bd
Dzheremilz <trempejeremie@gmail.com>
2021-02-19 13:57:11 +0100

    test

---
+++ crackme12.js
@@ -0,0 +1,13 @@
+// Vous devez assigner la bonne string à la variable password ci dessous
+// Et obtenir le message 'OK' en éxecutant le programme
+let password = ''
+
+const crackme12 = (password) => {
+  if (password === 'the‌‌ code') {
+    console.log('OK')
+  } else {
+    console.log('BAD')
+  }
+}
+
+crackme12(password)
