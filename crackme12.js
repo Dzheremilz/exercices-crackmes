cc745c53e5b7eb5f062db2a9dfb6ecaef2bbc83b
Dzheremilz <trempejeremie@gmail.com>
2021-02-19 16:54:16 +0100

    12 & 14

--- crackme12.js
+++ crackme12.js
@@ -2,8 +2,18 @@
 // Et obtenir le message 'OK' en éxecutant le programme
 let password = ''
 
+let tmp = ''
 const crackme12 = (password) => {
-  if (password === '') {
+  for (let i = 0; i < password.length; i++) {
+    if (i % 2 === 0) {
+      tmp += String.fromCharCode(((password.charCodeAt(i) + 29 - 65) % 26) + 97)
+    } else {
+      tmp += String.fromCharCode(((password.charCodeAt(i) + 55 - 65) % 26) + 65)
+    }
+    tmp = tmp.trim()
+  }
+  password = password.toUpperCase()
+  if (tmp.includes('rQgL') && password.includes('AHIS') && password.indexOf('ISGRA') && password.split('').slice(0, -13).reverse().join('') === 'ART' && tmp.search('uDfH') && password.length === 16) {
     console.log('OK')
   } else {
     console.log('BAD')
---
+++ crackme14.js
@@ -0,0 +1,15 @@
+// Vous devez assigner la bonne string à la variable password ci dessous
+// Et obtenir le message 'OK' en éxecutant le programme
+let password = 'password'
+
+const crackmeEasy = (password) => {
+  if (password === 'passw­ord') {
+    console.log('OK')
+  } else {
+    console.log('BAD')
+  }
+}
+
+crackmeEasy(password)
+
+// Sofiane s'est fait avoir, et vous ?
