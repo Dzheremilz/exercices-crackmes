c3187b9d546664814b147b242888d67d8e14db24
Dzheremilz <trempejeremie@gmail.com>
2021-02-19 13:00:57 +0100

    crack 11

---
+++ crackme11.js
@@ -0,0 +1,17 @@
+// Vous devez assigner la bonne string à la variable password ci dessous
+// Et obtenir le message 'OK' en éxecutant le programme
+let password = ''
+
+const crackme11 = (password) => {
+  let tmp1 = password.split('').slice(0, 4).reverse().join('')
+  console.log(tmp1)
+  let tmp2 = password.split('').slice(-4).reverse().join('')
+  console.log(tmp2)
+  if (tmp1 + tmp2 === 'password' && password.length > 8) {
+    console.log('OK')
+  } else {
+    console.log('BAD')
+  }
+}
+
+crackme11(password)
